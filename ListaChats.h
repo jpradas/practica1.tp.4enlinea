cbade0a52c2418f2a540f94b96ff6b1f8f0c2b83
jpradas <jpradas@ucm.es>
2014-05-13 18:04:45 +0200

    Update ListaChats.h

--- ListaChats.h
+++ ListaChats.h
@@ -1,27 +1,26 @@
-#ifndef LISTACHATS_H   
-#define LISTACHATS_H   
-      
-#include <string>  
-#include "Chat.h"   
-using namespace std;   
-      
-typedef tChat tChats [10];   
-      
-typedef struct {   
-    tChats chats;   
-    int contador;   
-} tListaChats;   
-      
-void cargarListaChats(tListaChats & chats, string id);   
-void guardarListaChats(tListaChats chats);   
-bool buscarChat(tListaChats & chats, int & pos, string nombre);   
-void EliminarPorPosicion(int & pos, tListaChats & chats);  
-void moverAlFinal(int & pos, tListaChats & chats); 
-void ordenarPorNombre(tListaChats & chats); 
-void ordenarPorFecha(tListaChats & chats); 
-void anadirAlFinal(tListaChats & chats, string nombre, string identificador); 
-void mostrarChats(tListaChats & ListaChats); 
-void NuevoChat(tListaChats & chats, string receptor, string identificador, int fecha); 
-      
-      
+#ifndef LISTACHATS_H  
+#define LISTACHATS_H  
+    
+#include <string> 
+#include "Chat.h"
+using namespace std;  
+    
+typedef tChat tChats [10];  
+    
+typedef struct {  
+    tChats chats;  
+    int contador;  
+} tListaChats;  
+    
+void cargarListaChats(tListaChats & chats, string id);  
+void guardarListaChats(tListaChats chats);  
+bool buscarChat(tListaChats & chats, int & pos, string nombre);  
+void EliminarPorPosicion(int & pos, tListaChats & chats); 
+void moverAlFinal(int & pos, tListaChats & chats);
+void ordenarPorNombre(tListaChats & chats);
+void ordenarPorFecha(tListaChats & chats);
+void mostrarChats(tListaChats & ListaChats);
+void NuevoChat(tListaChats & chats, tListaMensajes & ListaMensajes, string identificador);
+    
+    
 #endif
