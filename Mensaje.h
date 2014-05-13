16a9977f5b5e7abffc077dd29c2a783ebe2ae66d
jpradas <jpradas@ucm.es>
2014-05-13 18:05:11 +0200

    Update Mensaje.h

--- Mensaje.h
+++ Mensaje.h
@@ -1,26 +1,26 @@
-#ifndef MENSAJE_H  
-#define MENSAJE_H  
+#ifndef MENSAJE_H 
+#define MENSAJE_H 
+  
+#include <iostream>  
+#include <string> 
+#include <fstream>  
+using namespace std; 
+  
+typedef time_t tFecha;  
     
-#include <iostream>   
-#include <string>  
-#include <fstream>   
-using namespace std;  
-    
-typedef time_t tFecha;   
-      
-typedef struct {   
-    string emisor;   
-    string receptor;   
-    tFecha fecha;   
-    string texto;   
-} tMensaje;   
-    
-void mostrarFecha(tFecha fecha);   
-void mensajeNuevo(string emisor, string receptor, string texto, tMensaje & mensaje);   
-void mostrar(tMensaje mensaje);   
-void cargar(istream & archivo, tMensaje & mensaje);   
-void guardar(ostream & archivo, tMensaje & mensaje); 
-void mensajeBuzon(string emisor, string receptor, string texto, tMensaje & mensaje, int fecha); 
-    
-    
+typedef struct {  
+    string emisor;  
+    string receptor;  
+    tFecha fecha;  
+    string texto;  
+} tMensaje;  
+  
+void mostrarFecha(tFecha fecha);  
+void mensajeNuevo(string emisor, string receptor, string texto, tMensaje & mensaje);  
+void mostrar(tMensaje mensaje);  
+void cargar(istream & archivo, tMensaje & mensaje);  
+void guardar(ostream & archivo, tMensaje & mensaje);
+void mensajeBuzon(tMensaje & mensajeBuzon, tMensaje & mensajeChat);
+  
+  
 #endif
